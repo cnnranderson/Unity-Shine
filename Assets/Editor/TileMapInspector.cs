6c4538f6a5da071cc372b29cfb5743f2eede8a19
Conner <cnnranderson@gmail.com>
2015-12-12 12:21:34 -0800

    Initial Commit

---
+++ Assets/Editor/TileMapInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapInspector : Editor {
+
+	public override void OnInspectorGUI () {
+		DrawDefaultInspector ();
+		if (GUILayout.Button ("Regenerate")) {
+			TileMap map = (TileMap)target;
+			map.BuildMesh();
+		}
+	}
+}
---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+	private GameObject target;
+
+	public float cameraSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			Vector3 targetLocation = target.transform.position;
+			targetLocation.z = -10;
+			transform.position = Vector3.Lerp (transform.position, targetLocation, cameraSpeed * Time.deltaTime);
+		}
+	}
+}
